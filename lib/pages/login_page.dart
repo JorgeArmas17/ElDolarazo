2ad70c897d3899f17014b0dd3390863c77e14bb4
Grace <gvpujos>
2023-08-19 09:38:53 -0500

    Campos de entrada

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,30 +1,153 @@
 import 'package:flutter/material.dart';
+import 'package:el_dolarazo/components/my_button.dart';
+import 'package:el_dolarazo/components/my_textfield.dart';
+import 'package:el_dolarazo/components/square_tile.dart';
 
 class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+
+  // text editing controllers
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  // sign user in method
+  void signUserIn() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.grey[300],
       body: SafeArea(
         child: Center(
-          child: const Column(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SizedBox(height: 50),
+              const SizedBox(height: 50),
 
-              //logo
-              Icon(
+              // logo
+              const Icon(
                 Icons.lock,
                 size: 100,
               ),
 
-              SizedBox(height: 50),
+              const SizedBox(height: 50),
 
-              //Welcome back,
-              Text('Bienvenidos, te hemos extrañado'),
+              // welcome back, you've been missed!
+              Text(
+                'Bienvenido a "El Dolarazo"',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 16,
+                ),
+              ),
 
-              //username textfiedl
+              const SizedBox(height: 25),
 
-              //AÑADIR tus demas campos
+              // username textfield
+              MyTextField(
+                controller: usernameController,
+                hintText: 'Usuario',
+                obscureText: false,
+              ),
+
+              const SizedBox(height: 10),
+
+              // password textfield
+              MyTextField(
+                controller: passwordController,
+                hintText: 'Contraseña',
+                obscureText: true,
+              ),
+
+              const SizedBox(height: 10),
+
+              // forgot password?
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Olvide mi contraseña',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 25),
+
+              // sign in button
+              MyButton(
+                onTap: signUserIn,
+              ),
+
+              const SizedBox(height: 50),
+
+              // or continue with
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        'O Iniciar Sesión con:',
+                        style: TextStyle(color: Colors.grey[700]),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 50),
+
+            
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:  [
+                  // google button
+                  SquareTile(imagePath: 'lib/images/google.png'),
+
+                  SizedBox(width: 25),
+
+                  // apple button
+                  SquareTile(imagePath: 'lib/images/facebook.png')
+                ],
+              ),
+
+              const SizedBox(height: 50),
+
+              // not a member? register now
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Not a member?',
+                    style: TextStyle(color: Colors.grey[700]),
+                  ),
+                  const SizedBox(width: 4),
+                  const Text(
+                    'Register now',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
